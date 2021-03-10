01949afc3ce9b5fee013d0f526ce41ff6854f1ab
Anas-i-okasha <okasha.i.anas@gmail.com>
2021-03-10 20:53:45 +0200

    first task

---
+++ vuetodo/src/components/Addnew.vue
@@ -0,0 +1,32 @@
+<template>
+    <div>
+        <input placeholder="Enter your task here..." v-model="title">
+        <button @click="addtask()"> Add Task </button>
+        <ul v-bind:key="task.id" v-for="(task , index) in tasks">
+            <li>
+          <input type="checkbox">   {{task}}  <button @click="deletetask(index)"> Delete the Task </button>
+            </li>
+        </ul>
+    </div>
+</template>
+
+<script>
+export default {
+    name:"Addnew",
+    props:['tasks'],
+ data(){
+     return {
+         title:""
+     }
+ },
+ methods:{
+     addtask:function() {
+         this.tasks.push(this.title);
+         this.title="";
+     },
+     deletetask: function(index){
+         this.tasks.splice(index , 1)
+     }
+ }
+}
+</script>
---
+++ vuetodo/src/components/HelloWorld.vue
@@ -0,0 +1,27 @@
+<template>
+  <div >
+    <h2> Todo list</h2>
+    <div>
+      <Addnew v-bind:tasks="tasks"/>
+    </div>
+  </div>
+</template>
+
+<script>
+import Addnew from './Addnew'
+export default {
+  name: 'HelloWorld',
+  components:{
+    Addnew
+  },
+  data(){
+    return {
+      tasks:[
+
+      ],
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
